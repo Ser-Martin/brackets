90deddb22a59f766e679e0cd31df8c5324dbc5c8
Sergiy Martynov <martins.codespace.kh@gmail.com>
2022-08-12 09:20:39 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const mirror = (ch) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i].includes(ch)) return bracketsConfig[i][0];
+    }
+  };
+
+  const stack = [];
+
+  const isSame = (ch) => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (
+        ch === bracketsConfig[i][1] &&
+        ch === bracketsConfig[i][0] &&
+        stack.at(-1) !== ch
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  let opens = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] !== bracketsConfig[i][1])
+      opens.push(bracketsConfig[i][0]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+
+    if (isSame(current) || opens.includes(current)) {
+      stack.push(current);
+    } else {
+      let topElement = stack.at(-1);
+
+      if (topElement === mirror(current)) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
